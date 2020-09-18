7dcb16f409ead5f75be641a6bb329cd8849fceaf
Patrick Li <lipatrick.1023@gmail.com>
2020-09-17 20:32:53 -0400

    Implemented zmq for Python - C IPC; optimized send.py

--- Python/main.py
+++ Python/main.py
@@ -1,19 +1,28 @@
 from solace import *
+import zmq
          
 client = initialize()
+
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.bind("tcp://*:5556")
+
+def parse_3_axis(str):
+    vals = str.split(" ")
+    if len(vals) != 3:
+        return -1, -1, -1
+    return vals[0], vals[1], vals[2]
+
 while True:
     while client.message_received != True:
-        time.sleep(0.01)
+        time.sleep(0.2)
 
     client.message_received = False
     # print(client.message_contents)
     if client.message_topic == "joystick_axis":
         print(client.message_contents)
 
-        # opening a file in 'w'
-        file = open('../vJoySDK/SDK/src/joystick_axis.txt', 'w')
-        # write() - it used to write direct text to the file
-        # writing data using the write() method
-        file.write(client.message_contents)
-        # closing the file
-        file.close()
+        x, y, z = parse_3_axis(client.message_contents)
+
+        socket.send_string("%s %s %s" % (x, y, z))
+        print("sent " + str(x) + " " + str(y) + " " + str(z))
--- Python/send.py
+++ Python/send.py
@@ -1,38 +1,46 @@
 from solace import *
 import keyboard  # using module keyboard
+from time import sleep
 
 x, y, z = 0, 0, 0
          
 client = initialize()
 
 while True:
+    changed = False
     if keyboard.is_pressed('w'):  # if key 'w' is pressed 
-        y += 20
+        y += 200
         print("y = " + str(y) + "\n")
+        changed = True
         #break  # finishing the loop
     if keyboard.is_pressed('s'):  # if key 's' is pressed 
-        y -= 20
+        y -= 200
         print("y = " + str(y) + "\n")
+        changed = True
         #break  # finishing the loop
     if keyboard.is_pressed('a'):  # if key 'a' is pressed 
-        x -= 20
+        x -= 200
         print("x = " + str(x) + "\n")
+        changed = True
         #break  # finishing the loop
     if keyboard.is_pressed('d'):  # if key 'd' is pressed 
-        x += 20
-        print("x = " + str(x) + "\n")     
+        x += 200
+        print("x = " + str(x) + "\n") 
+        changed = True    
 
         #break  # finishing the loop
     if keyboard.is_pressed('q'):  # if key 'q' is pressed 
-        z -= 20
+        z -= 200
         print("z = " + str(z) + "\n")
+        changed = True
         #break  # finishing the loop
     if keyboard.is_pressed('e'):  # if key 'e' is pressed 
-        z += 20
+        z += 200
         print("z = " + str(z) + "\n")
+        changed = True
         #break  # finishing the loop
 
-    client.publish('joystick_axis', payload = str(x) + ' ' + str(y) + ' ' + str(z))
-    print(str(x) + ' ' + str(y) + ' ' + str(z))
-
-    # print(str(x) + ' ' + str(y) + ' ' + str(z))
+    if changed:
+        client.publish('joystick_axis', payload = str(x) + ' ' + str(y) + ' ' + str(z))
+        print(str(x) + ' ' + str(y) + ' ' + str(z))
+    sleep(0.01)
